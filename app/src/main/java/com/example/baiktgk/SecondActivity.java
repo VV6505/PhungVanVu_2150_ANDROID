12dcbceea2f3419930e93fc671034c799d3c35e4
VV6505 <phungvu065005@gmail.com>
2025-10-15 08:37:11 +0700

    upload code 15052025 2150

--- app/src/main/java/com/example/baiktgk/SecondActivity.java
+++ app/src/main/java/com/example/baiktgk/SecondActivity.java
@@ -2,23 +2,23 @@ package com.example.baiktgk;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class SecondActivity {
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_main);
-//        editText = findViewById(R.id.ed_email150);
-//        editPass = findViewById(R.id.edpass_150);
-//
-//        btn_login150.setOnClickListener(v -> {
-//            String email = editText.getText().toString().trim();
-//            Intent intent = new Intent(MainActivity.this,SecondActivity.class);
-//            intent.putExtra("email",email);
-//            startActivity(intent);
-//        });
-//    }
+public class SecondActivity extends AppCompatActivity {
+    private TextView back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.fgm_second);
+        back = findViewById(R.id.tv_back150);
+
+            back.setOnClickListener(v -> {
+            Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
 }
